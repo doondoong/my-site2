44288181a7148f53f5cb1b0dea289ba9f114e343
개발하는 김대리 <86033303+doondoong@users.noreply.github.com>
2021-06-30 12:35:04 +0900

    Add files via upload

--- index.php
+++ index.php
@@ -143,6 +143,17 @@
                         <i class="fas fa-download me-2"></i>
                         Free Download!
                     </a>
+                    <div id="map" style="width:500px;height:400px;"></div>
+                    <script type="text/javascript" src="//dapi.kakao.com/v2/maps/sdk.js?appkey=5559ee2cb69f113b5b1a42efc1079a70"></script>
+                    <script>
+                        var container = document.getElementById('map');
+                        var options = {
+                            center: new kakao.maps.LatLng(33.450701, 126.570667),
+                            level: 3
+                        };
+
+                        var map = new kakao.maps.Map(container, options);
+                    </script>
                 </div>
             </div>
         </section>
